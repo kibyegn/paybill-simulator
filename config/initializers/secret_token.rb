1b4ac321cc503a3daa1aed98f205a0db4e7649b7
Kibet Yegon <kibyegn@gmail.com>
2011-05-06 17:25:19 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaybillSimulator::Application.config.secret_token = '537b2994b16381520bd4ba79884ba9ddcebc5ff93af5fee863981c5d4cee70f1c7491f95253007b48405d23ab9dc265249643a86056bfc0066b715baa9015f51'
