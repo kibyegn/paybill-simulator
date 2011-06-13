8b874dc7fd8f5466b09c26ea0c2922183b477c01
Kibet Yegon <kibyegn@gmail.com>
2011-06-13 15:44:52 +0300

    added events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110509090658) do
+ActiveRecord::Schema.define(:version => 20110602094046) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.date     "day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "transactions", :force => true do |t|
     t.string   "mp_transaction_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Transaction.delete_all
+
 BILLER_BUSINESS_NUMBER = ['555555', '666666', '777777']
-ACCOUNT_NUMBERS = ['0123456789', '9876543210', '0123498765']
+ACCOUNT_NUMBERS = ['0123456789', '9876543210', '0123498765', '8292367325', '8371367349']
 SENDER_FIRST_NAMES = ['John', 'Steve', 'Mary', 'Jane']
 SENDER_LAST_NAMES = ['Karanja', 'Simiyu', 'Wambua', 'Rop']
 SENDER_MIDDLE_NAMES = ['S.', 'N.', 'B.', 'R']
@@ -18,21 +20,21 @@ SENDER_MIDDLE_NAMES = ['S.', 'N.', 'B.', 'R']
   :description => "Pending")
 
 Transaction.create(:mp_transaction_id  => rand(9999), :biller_business_number =>BILLER_BUSINESS_NUMBER.first,
-  :transaction_date => Date.today, :transaction_type => "Paybill", :account_number => ACCOUNT_NUMBERS.first,
+  :transaction_date => Date.today, :transaction_type => "Paybill", :account_number => ACCOUNT_NUMBERS[1],
   :sender_msisdn => "072064647", :sender_first_name => SENDER_FIRST_NAMES[1], :sender_middle_name =>
   SENDER_MIDDLE_NAMES[1], :sender_last_name => SENDER_LAST_NAMES[1], :amount => "700", :currency => "KES", :status => "0",
   :description => "Pending")
 
 Transaction.create(:mp_transaction_id  => rand(9999), :biller_business_number =>BILLER_BUSINESS_NUMBER.first,
-  :transaction_date => Date.today, :transaction_type => "Paybill", :account_number => ACCOUNT_NUMBERS.first,
+  :transaction_date => Date.today, :transaction_type => "Paybill", :account_number => ACCOUNT_NUMBERS[2],
   :sender_msisdn => "0710647439", :sender_first_name => SENDER_FIRST_NAMES[2], :sender_middle_name =>
   SENDER_MIDDLE_NAMES[2], :sender_last_name => SENDER_LAST_NAMES[2], :amount => "1600", :currency => "KES", :status => "0",
   :description => "Pending")
 
 Transaction.create(:mp_transaction_id  => rand(9999), :biller_business_number =>BILLER_BUSINESS_NUMBER.first,
-  :transaction_date => Date.today, :transaction_type => "Paybill", :account_number => ACCOUNT_NUMBERS.first,
-  :sender_msisdn => "0723456789", :sender_first_name => SENDER_FIRST_NAMES[0], :sender_middle_name =>
-  SENDER_MIDDLE_NAMES[0], :sender_last_name => SENDER_LAST_NAMES[0], :amount => "1900", :currency => "KES", :status => "0",
+  :transaction_date => Date.today, :transaction_type => "Paybill", :account_number => ACCOUNT_NUMBERS[3],
+  :sender_msisdn => "0728837221", :sender_first_name => SENDER_FIRST_NAMES[3], :sender_middle_name =>
+  SENDER_MIDDLE_NAMES[3], :sender_last_name => SENDER_LAST_NAMES[3], :amount => "1900", :currency => "KES", :status => "0",
   :description => "Pending")
 
 #def seed_transactions
